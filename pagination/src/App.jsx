0d5eebc32a9beea0ea148a6be8779ff41978907a
Sanjana <sanjana6122002@gmail.com>
2025-09-03 21:48:23 +0530

    pagination

--- pagination/src/App.jsx
+++ pagination/src/App.jsx
@@ -3,17 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import BasicPagination from './component/BasicPagination'
+import OutlinedPagination from './component/OutlinedPagination'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-   <h1 className='text-4xl text-center font-semibold m-4  underline'>Reusable Pagination Components</h1>
+      <h1 className='text-4xl text-center font-semibold m-4  underline'>Reusable Pagination Components</h1>
 
-    <div className='m-8'>
+      <div className='m-8'>
         <h1 className='text-center text-2xl'>Basic Pagination </h1>
-         <BasicPagination />
+
+        <div className='m-8'>
+          <BasicPagination />
+        </div>
+
+        <div className='m-8'>
+          <OutlinedPagination />
+        </div>
+
       </div>
 
     </>
