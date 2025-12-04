a69b91a3adacd2b78471fe53cd0939a43f819c94
Sanjana <sanjana6122002@gmail.com>
2025-12-04 16:03:40 +0530

    appbar

---
+++ AppBar/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+       <h1 className="text-4xl text-center font-medium m-4">AppBar Components</h1>
+    </>
+  )
+}
+
+export default App
