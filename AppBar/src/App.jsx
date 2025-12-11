cc435ec0a7663f00760f8eb71c44ac8a5a60c11a
Sanjana <sanjana6122002@gmail.com>
2025-12-11 21:10:34 +0530

    appbar

--- AppBar/src/App.jsx
+++ AppBar/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import AppBarWithMenu from './components/AppBarWithMenu'
 import BasicAppbar from './components/BasicAppbar'
 
 function App() {
@@ -10,6 +11,11 @@ function App() {
        <div className='mt-8 bg-blue-50 rounded-xl shadow-md p-4'>
         <h1 className='text-center text-2xl p-4 underline'>Basic Transition</h1>
         <BasicAppbar/>
+      </div>
+
+        <div className='mt-8 bg-blue-50 rounded-xl shadow-md p-4'>
+        <h1 className='text-center text-2xl p-4 underline'>Basic Transition</h1>
+        <AppBarWithMenu/>
       </div>
     </>
   )
---
+++ AppBar/src/components/AppBarWithMenu.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AppBarWithMenu() {
+  return (
+    <div>AppBarWithMenu</div>
+  )
+}
+
+export default AppBarWithMenu
