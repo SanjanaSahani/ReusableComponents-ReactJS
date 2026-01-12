d78d8a79a55df9ab1becfb0514a8eb5c0bf905ca
Sanjana <sanjana6122002@gmail.com>
2026-01-12 14:04:54 +0530

    Login Page

---
+++ LoginPage/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <h1 className='text-4xl text-center font-semibold m-4  underline'>Login Page</h1>
+
+      <div className='m-8 '>
+        <h1 className='text-center text-2xl'>Login Page</h1>
+        <div className='flex items-center justify-center bg-gradient-to-r from-blue-50 via-purple-50 to-pink-50 cursor-pointer'>
+        <h1>hello</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
